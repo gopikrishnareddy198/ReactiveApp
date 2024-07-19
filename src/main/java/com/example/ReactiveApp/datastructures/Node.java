cfb6454370c1b74f0eb1376bf7e94207f1fa7c1e
gopikrishnareddy198 <gopikrishnareddy198@gmail.com>
2024-07-19 10:18:32 -0500

    Added datastructures

---
+++ src/main/java/com/example/ReactiveApp/datastructures/Node.java
@@ -0,0 +1,13 @@
+package com.example.ReactiveApp.datastructures;
+
+public class Node
+{
+    public int info;
+    public Node link;
+
+    public Node(int i)
+    {
+        info=i;
+        link=null;
+    }
+}
